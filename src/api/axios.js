ef2524ca75f5f65d0b9576395adcad3fdf5b237d
Sudhir Rai <sudhirrai128@gmail.com>
2024-05-08 23:37:34 +0530

    Fix git push

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-<<<<<<< HEAD
     baseURL: "https://noteapp-backend-2wsd.onrender.com",
     // baseURL: 'http://localhost:9000',
-=======
-    baseURL: "https://noteapp-backend-ul25.onrender.com",
->>>>>>> e59b1b9b7b106f8aa74eecc072583ec3d475b22f
     withCredentials: true,
 })
